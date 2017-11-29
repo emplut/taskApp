eb4b17907a2aece912608fd5004bffb9733f8ebd
michalplu <mplucinski90@gmail.com>
2017-11-29 18:21:26 +0100

    taskApp

--- src/main/java/com/sda/demo/tasks/facade/TaskFacade.java
+++ src/main/java/com/sda/demo/tasks/facade/TaskFacade.java
@@ -1,16 +1,21 @@
 package com.sda.demo.tasks.facade;
 
+import com.sda.demo.tasks.TasksApplication;
 import com.sda.demo.tasks.model.Task;
 import com.sda.demo.tasks.repository.TaskRepository;
+import org.apache.log4j.spi.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class TaskFacade {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TasksApplication.class);
 
     @Autowired
     private TaskRepository taskRepository;
@@ -22,8 +27,14 @@ public class TaskFacade {
     public void save(Task task) {
         if (task.getDateCreated() == null) {
             task.setDateCreated(LocalDateTime.now());
+        } else{
+            LOGGER.info("Saving edited task with id: {} and name {}",task.getName());
         }
-        taskRepository.save(task);
+        try{
+            taskRepository.save(task);
+        }catch (Exception ex){
+            LOGGER.error("Saving task error" , ex.getMessage());
+        }
     }
 
     public Task findTaskById(Long id) {
@@ -32,5 +43,6 @@ public class TaskFacade {
 
     public void deleteTask(Long id) {
         taskRepository.delete(id);
+        LOGGER.info("Deleted task id: {}");
     }
 }
